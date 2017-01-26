486fcab693cb7863c20d723ebc19c880037cb75c
rudolfps <rudolfps@gmail.com>
2017-01-26 16:50:29 +0100

    tttt init

---
+++ src/com/greenfox/memorycardgame/gui/Main.java
@@ -0,0 +1,26 @@
+package com.greenfox.memorycardgame.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by ${rudolfps} on 2017.01.26.
+ */
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("memory.fxml"));
+        primaryStage.setTitle("Memory Game");
+        primaryStage.setScene(new Scene(root, 500, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ test/com/greenfox/memorycardgame/memory/ImageFileProviderTest.java
@@ -0,0 +1,29 @@
+package com.greenfox.memorycardgame.memory;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Created by ${rudolfps} on 2017.01.26..
+ */
+public class ImageFileProviderTest {
+
+
+    @Test
+    public void testGetImageCount() throws Exception {
+        ImageFileProvider provider = new ImageFileProvider("");
+
+        Assert.assertTrue(0 < provider.getImageCount());
+    }
+
+    @Test
+    public void testGetImageFiles() throws Exception {
+        ImageFileProvider provider = new ImageFileProvider("");
+
+        File[] imageFiles = provider.getImageFiles();
+        Assert.assertNotNull(imageFiles);
+        Assert.assertTrue(0 < imageFiles.length);
+    }
+}
